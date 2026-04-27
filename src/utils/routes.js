ed6a7f24857cd0cf5770389ab346fca61fb56d60
OyanibTech-iii <pacificooyanib@gmail.com>
2026-04-27 20:52:11 +0800

    feat: components and utilities intigration

---
+++ src/utils/routes.js
@@ -0,0 +1,12 @@
+export default {
+  // Auth
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+
+  // Main
+  HOME: 'Home',
+  PROFILE: 'Profile',
+  PRODUCT: 'Product',
+  TERMS_POLICY: 'TermsPolicy',
+  COURSES: 'Courses',
+};
